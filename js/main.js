bd2699dfb6162bfefcbede8470689c7dd99766a3
Trey Mosley <iiimosley@treys-mbp.attlocal.net>
2018-01-01 21:34:57 -0600

    js edits

--- js/main.js
+++ js/main.js
@@ -4,13 +4,18 @@ let flexNav = document.querySelector("#flexNav");
 if (document.body.clientWidth < 577) {
     flexNav.style.display = "none";
     let navDrop = document.createElement('select');
+    navDrop.setAttribute("id", "dropNav");
     nav.appendChild(navDrop);
-    console.log(flexNav.children[2].innerHTML);
+    console.log(navDrop);
     for (let i = 0; i < flexNav.children.length; i++) {
         let navOption = document.createElement('option');
         navOption.innerHTML = flexNav.children[i].innerHTML;
+        navOption.value = flexNav.children[i].firstChild.attributes[0].value
+        console.log(navOption);
         navDrop.appendChild(navOption);
     }
 }
+
+
 
 // document.querySelector("#flexNav")
