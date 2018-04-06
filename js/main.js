3ef22409dffc60c184405a46d1b1dd05f78ec08a
Trey Mosley <iiimosley@gmail.com>
2018-04-06 02:10:03 -0500

    sweet hover animation

--- js/main.js
+++ js/main.js
@@ -10,6 +10,16 @@ $("#dropDown").click(function () {
     $("#dropContain").slideToggle(400, "swing");
 });
 
+const selectTab = (e) => {
+    $("#flexNav>li>a").removeClass("activeTab");
+    $(e.target).addClass("activeTab");
+};
+
+$("#flexNav").on('click', (e)=>{
+    selectTab(e);
+});
+
+
 $(document).on('click', '#aboutLink, #aboutMobile', aboutView);
 
 $(document).on('click', '#portfolioLink, #portfolioMobile', portfolioView);
