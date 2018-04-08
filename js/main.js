41a727ccd917bc3ca88badb077e7b0d43224ca7e
Trey Mosley <iiimosley@gmail.com>
2018-04-08 18:05:38 -0500

    path fill transition (footerNav); rm dropdown

--- js/main.js
+++ js/main.js
@@ -6,10 +6,6 @@ const Handlebars = require('hbsfy/runtime');
 const { aboutView, portfolioView, contactView } = require('./view');
 
 
-$("#dropDown").click(function () {
-    $("#dropContain").slideToggle(400, "swing");
-});
-
 const selectTab = (e) => {
     $("#flexNav>li>a").removeClass("activeTab");
     $(e.target).addClass("activeTab");
@@ -20,11 +16,7 @@ $("#flexNav").on('click', (e)=>{
 });
 
 $('#stickyFooter>div').on('click', (e)=>{
-    console.log(e.currentTarget.id);
     $(`#${e.currentTarget.id}>svg>path`).css('fill', 'white');
-    // [...e.target.children].forEach(icon => {
-    //     icon.style.fill = 'white';
-    // });
     $(`#stickyFooter>div:not(#${e.currentTarget.id})>svg>path`).css('fill', 'black');
 });
 
