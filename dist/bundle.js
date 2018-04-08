65711bf4c611fc7b150d22d4fedbe09539e0282f
Trey Mosley <iiimosley@gmail.com>
2018-04-08 18:36:35 -0500

    flexHead click removes tab styling

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,15 +6,21 @@ const Handlebars = require('hbsfy/runtime');
 
 const { homeView, aboutView, portfolioView, contactView } = require('./view');
 
+const deselectTab = () => {
+    $("#flexNav>li>a").removeClass("activeTab");
+};
 
 const selectTab = (e) => {
-    $("#flexNav>li>a").removeClass("activeTab");
+    // $("#flexNav>li>a").removeClass("activeTab");
+    deselectTab();
     $(e.target).addClass("activeTab");
 };
 
 $("#flexNav").on('click', (e)=>{
     selectTab(e);
 });
+
+$("#flexHead").on('click', deselectTab);
 
 $('#stickyFooter>div').on('click', (e)=>{
     $(`#${e.currentTarget.id}>svg>path`).css('fill', 'white');
